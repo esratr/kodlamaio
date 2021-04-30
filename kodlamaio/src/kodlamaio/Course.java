40ad6ae04c1e158ea757d1e4a930b46bc609a5a4
ESRA TIRPANCI <ESRA TIRPANCI@esra.local>
2021-05-01 00:21:25 +0300

    Java&React-Week2

---
+++ kodlamaio/src/kodlamaio/Course.java
@@ -0,0 +1,20 @@
+package kodlamaio;
+
+public class Course {
+	
+	public Course(int id, String courseName ,String courseTeacher,String detail,int price ) {
+		this.id=id;
+		this.courseName=courseName;
+		this.courseTeacher=courseTeacher;
+		this.price=price;
+	}
+	
+	int id;
+	String courseName;
+	String courseTeacher;
+	String detail;
+	int price;
+	
+	
+
+}
---
+++ kodlamaio/src/kodlamaio/StudentManager.java
@@ -0,0 +1,10 @@
+package kodlamaio;
+
+public class StudentManager {
+	public void addToStudent(Student student) {
+		System.out.println(student.name + " " + student.surname + " isimli öğrenci sisteme eklenmiştir. ");
+		}
+	public void deleteFromStudent(Student student) {
+		System.out.println(student.name + " " + student.surname + " isimli öğrenci sistemden silinmiştir. ");
+		}
+}
