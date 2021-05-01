b4fd76faf736cb88e23283523549fbbcd47a8689
ESRA TIRPANCI <ESRA TIRPANCI@esra.local>
2021-05-02 01:15:56 +0300

    inheritance

---
+++ inheritance2/src/inheritance2/CustomerManager.java
@@ -0,0 +1,11 @@
+package inheritance2;
+
+public class CustomerManager {
+	
+	public void add(Logger logger) { //bana loggerýn ne olduðunu söyle
+		System.out.println("müþteri eklendi");
+	
+		logger.log();
+	}
+
+}
